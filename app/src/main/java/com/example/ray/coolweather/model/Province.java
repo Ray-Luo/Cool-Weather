df3519d8f9f3d95d374d8ceb1e63f70b9a4d3c37
Ray-Luo <leiluo.ray@gmail.com>
2015-09-25 01:06:48 -0700

    Second commit.

---
+++ app/src/main/java/com/example/ray/coolweather/model/Province.java
@@ -0,0 +1,30 @@
+package com.example.ray.coolweather.model;
+
+/**
+ * Created by Ray on 9/23/2015.
+ */
+public class Province {
+
+    private int id;
+    private String provinceName;
+    private String provinceCode;
+
+    public int getId()
+    {return id;}
+
+    public void setId(int id)
+    {this.id = id;}
+
+    public String getProvinceName()
+    {return provinceName;}
+
+    public void setProvinceName(String provinceName)
+    {this.provinceName = provinceName;}
+
+    public String getProvinceCode()
+    {return provinceCode;}
+
+    public void setProvinceCode(String provinceCode)
+    {this.provinceCode = provinceCode;}
+
+}
