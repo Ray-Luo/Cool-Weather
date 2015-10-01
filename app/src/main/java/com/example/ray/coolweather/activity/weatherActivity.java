5726c03b3e9a9ea8b82ba0506a800cbc5e7658e0
Ray-Luo <leiluo.ray@gmail.com>
2015-10-01 00:20:43 -0700

    新增切换城市和手动更新天气功能

---
+++ app/src/main/java/com/example/ray/coolweather/activity/weatherActivity.java
@@ -0,0 +1,89 @@
+package com.example.ray.coolweather.activity;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.preference.PreferenceManager;
+import android.text.TextUtils;
+import android.view.View;
+import android.view.Window;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import com.example.ray.coolweather.R;
+import com.example.ray.coolweather.db.CoolWeatherDB;
+
+import org.w3c.dom.Text;
+
+import java.io.InputStream;
+
+/**
+ * Created by Ray on 9/30/2015.
+ */
+public class weatherActivity extends Activity implements onClickListener{
+
+    private LinearLayout weatherInfoLayout;
+    private TextView cityNameText;
+    private TextView publishText;
+    private TextView weatherDespText;
+    private TextView temp1Text;
+    private TextView temp2Text;
+    private TextView currentDateText;
+    private Button switchCity;
+    private Button refreshWeather;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        setContentView(R.layout.weather_layout);
+        weatherInfoLayout = (LinearLayout)findViewById(R.id.weather_info_layout);
+        cityNameText = (TextView)findViewById(R.id.city_name);
+        publishText = (TextView)findViewById(R.id.publish_text);
+        weatherDespText = (TextView)findViewById(R.id.weather_desp);
+        temp1Text = (TextView) findViewById(R.id.temp1);
+        temp2Text = (TextView) findViewById(R.id.temp2);
+        currentDateText = (TextView) findViewById(R.id.current_date);
+        switchCity = (Button) findViewById(R.id.switch_city);
+        refreshWeather = (Button) findViewById(R.id.refresh_weather);
+        String countyCode = getIntent().getStringExtra("county_code");
+        if(!TextUtils.isEmpty(countyCode))
+        {
+            publishText.setText("同步中...");
+            weatherInfoLayout.setVisibility(View.INVISIBLE);
+            cityNameText.setVisibility(View.INVISIBLE);
+            queryWeatherCode(countyCode);
+        }
+        else
+        {showWeather();}
+        switchCity.setOnClickListener(this);
+        refreshWeather.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v)
+    {
+        switch (v.getId())
+        {
+            case R.id.switch_city:
+                Intent intent = new Intent(this, ChooseAreaActivity.class);
+                intent.putExtra("from_weather_activity", true);
+                startActivity(intent);
+                finish();
+                break;
+            case R.id.refresh_weather:
+                publishText.setText("同步中...");
+                SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(this);
+                String weatherCode = prefs.getString("weather_code","");
+                if(!TextUtils.isEmpty(weatherCode))
+                    queryWeatherInfo(weeatherCode);
+                break;
+            default:
+                break;
+        }
+    }
+
+}
